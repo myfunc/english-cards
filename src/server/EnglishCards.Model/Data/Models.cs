453d9e99bef657fa55b8d4b1a4fbed1d88ac664b
Denys Myronov <mironov.link@gmail.com>
2020-05-17 23:29:01 +0300

    Added Model solution

---
+++ src/server/EnglishCards.Model/Data/Models.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace EnglishCards.Model.Data
+{
+    class Group
+    {
+        [Key]
+        public string Id { get; set; }
+    }
+
+    class User
+    {
+
+    }
+
+    class UserInGroup
+    {
+
+    }
+
+    class ProgressData
+    {
+
+    }
+
+    class Word
+    {
+
+    }
+
+    class WordTranslation
+    {
+
+    }
+
+    class WordGroup
+    {
+
+    }
+}
