77da962220a2fe6b2c0648d29293b5137265dcec
Denys Myronov <mironov.link@gmail.com>
2020-05-17 23:10:04 +0300

    Added VS solution

---
+++ src/server/EnglishCards.Host/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using ServerHost = Microsoft.Extensions.Hosting.Host;
+
+namespace EnglishCards.Host
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            ServerHost.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
