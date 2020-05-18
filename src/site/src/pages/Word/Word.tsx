80e203aec6078fe2d8120242c5ee09d4e7718da9
Olena Momotenko <derry@zeoalliance.com>
2020-05-18 09:33:16 +0300

    add animation to slider

--- src/site/src/pages/Word/Word.tsx
+++ src/site/src/pages/Word/Word.tsx
@@ -3,8 +3,6 @@ import { Card, Slide, Title, Wrapper } from "../../components";
 import { NavLink } from "react-router-dom";
 
 export function Word(props: any) {
-    
-
     const arr = [
         { id: "1", nativeTranslation: "искра", foreignTranslations: ["spark"], imageUrl: "https://image.flaticon.com/icons/svg/773/773058.svg" },
         { id: "2", nativeTranslation: "часы", foreignTranslations: ["watch"], imageUrl: "https://image.flaticon.com/icons/svg/2922/2922319.svg" },
@@ -13,21 +11,26 @@ export function Word(props: any) {
     ];
 
     const [active, isActive] = useState(0);
-    let slides = arr.map(item => ({active : false}));
+    const [direction, isDirection] = useState("");
+    let slides = arr.map(item => ({active : false, direction: "" }));
    
     slides[active].active = true;
+    slides[active].direction = `-show-to-${direction}`;
 
     const prevHandler = () => {
         active === 0 ? isActive(arr.length - 1) : isActive(active - 1);
+        isDirection("left");
     }
 
     const nextHandler = () => {
         active < arr.length - 1 ? isActive(active + 1) : isActive(0);
+        isDirection("right");
     }
 
     useEffect(() => {
         slides[active].active = true;
-    }, [active, slides]);
+        slides[active].direction = `-show-to-${direction}`;
+    }, [active, direction, slides]);
 
     return (
         <div className="word">
@@ -38,9 +41,11 @@ export function Word(props: any) {
                 <div className="word__content slider">
                     <button className="slider__control slider__control_prev" onClick={prevHandler}  />
                     <button className="slider__control slider__control_next" onClick={nextHandler} />
-                    {arr.map((item, index) => 
-                        <Slide key={index} active={slides[index].active}><Card key={item.id} {...item} /></Slide>
-                   )} 
+                    <div className="slider__content">
+                        {arr.map((item, index) => 
+                            <Slide key={index} active={slides[index].active} direction={slides[index].direction}><Card key={item.id} {...item} /></Slide>
+                        )} 
+                    </div>
                 </div>
             </Wrapper>
         </div>
